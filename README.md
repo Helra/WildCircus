1c3c2486b8ce43507c14b3b2d46825a858a19b81
Caroline Richard <rchrdcaroline@gmail.com>
2020-01-31 09:01:01 +0100

    last commit

--- README.md
+++ README.md
@@ -7,14 +7,13 @@
 4. Créer style de la partie Show de la homepage - OK
 5. Créer CRUD event - OK
 6. Créer Fixture - OK
-7. Créer style de la partie Event de la homepage - en cours
-7.5 Polish la HomePage - en cours
-8. Créer l'administration de la partie Show
-9. Créer l'administration de la partie Event
+7. Créer style de la partie Event de la homepage - OK
+7. Polish la HomePage - en cours
+8. Créer l'administration de la partie Show - Ok
+9. Créer l'administration de la partie Event - Ok
 10. Responsive Partie Présentation
 11. Responsive Partie Event
 12. Responsive Partie Spectacle
 13. Show all Spectacle
 14. Faire le dossier Ressources
-15. Authentification ???
 
--- src/Form/SpectacleType.php
+++ src/Form/SpectacleType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Spectacle;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,10 @@ class SpectacleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('presentation')
-            ->add('image')
-            ->add('price')
+            ->add('name', TextType::class, ['label'=> 'Nom du Spectacle'])
+            ->add('presentation', TextareaType::class, ['label'=>'Présentation'])
+            ->add('image', TextType::class, ['label' => 'Image(URL)'])
+            ->add('price', NumberType::class, ['label' => 'Prix'])
         ;
     }
 
